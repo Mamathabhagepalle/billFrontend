801796a547809dd027483a48227c2b238b610d7b
Mamathabhagepalle <mamathabhagepalle@gmail.com>
2025-06-18 15:10:43 +0530

    register page styled

--- src/componets/user/Register.jsx
+++ src/componets/user/Register.jsx
@@ -2,33 +2,34 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className='bg-[#efefef] size-full flex justify-center items-center'>
-      <form action="" className='bg-amber-200 h-1/2 w-1/2 flex justify-center items-center flex-col'>
-        <div className='bg-red-500'>
-          <h1>registration</h1>
-        </div>
-        <div>
-          <input type="text" name="" placeholder='Enter Name'/>
+    <div className='bg-[#efefef] size-full flex justify-center items-center rounded-2xl' >
+      <form action="" className='bg-whight h-2/3  w-1/2 flex justify-center items-center flex-col gap-4 px-[80px] py-40 shadow-2xl rounded-3xl'>
+        <div className='font-bold  flex justify-center items-center text-3xl '>
+          <h1>Registration Form</h1>
         </div>
 
-        <div>
-          <input type="text" name="" placeholder='Enter UserName'/>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+          <input type="text" name="" placeholder='Enter Name' className='w-full outline-none px-4 h-10'/>
         </div>
 
-        <div>
-          <input type="email" name="" placeholder='Enter Email'/>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+          <input type="text" name="" placeholder='Enter UserName' className='w-full outline-none px-4 h-10'/>
         </div>
 
-        <div>
-          <input type="password" name="" placeholder='Enter password'/>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+          <input type="email" name="" placeholder='Enter Email' className='w-full outline-none px-4 h-10'/>
         </div>
 
-        <div>
-          <input type="password" name="" placeholder='Enter repeate password'/>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+          <input type="password" name="" placeholder='Enter password' className='w-full outline-none px-4 h-10'/>
         </div>
 
-        <div>
-          <button>Click</button>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+          <input type="password" name="" placeholder='Enter repeate password' className='w-full outline-none px-4 h-10'/>
+        </div>
+
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-md bg-black '>
+          <button className='w-full outline-none px-4 h-10 text-white font-bold'>Click</button>
         </div>
       </form>
     </div>
