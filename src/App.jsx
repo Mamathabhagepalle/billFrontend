c5a62ffe3077bade46ac30f877b3123776ba735f
Mamathabhagepalle <mamathabhagepalle@gmail.com>
2025-06-20 14:02:05 +0530

    form validation

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes/Routes'
 import "./App.css"
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
     <RouterProvider router={routes}>
-
+      
     </RouterProvider>
   )
 }
--- src/componets/user/Register.jsx
+++ src/componets/user/Register.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
 import { MdDriveFileRenameOutline } from "react-icons/md";
 import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
 import { MdAlternateEmail } from "react-icons/md";
 import { TbPasswordFingerprint } from "react-icons/tb";
 import { TbLockPassword } from "react-icons/tb";
+import {validatePassword} from "val-pass"
 
 const Register = () => {
   let [state,setState]=useState({
@@ -13,20 +15,55 @@ const Register = () => {
     password:""
   })
 
+  let [repeatPassword,setReapeatePassword]=useState("")
+
+  let[errorMessage,setErrorMessage]=useState("")
   
       let handleChange=(e)=>{
           let {name,value}=e.target
+          if(name=="password"){
+            let {validateAll,getAllValidationErrorMessage}=validatePassword(value,8)
+
+            validateAll()?setErrorMessage(""):setErrorMessage(getAllValidationErrorMessage())
+            value==""&&setErrorMessage("")
+          }
           setState({...state,[name]:value})
       }
 
       let handleSubmit=(e)=>{
         e.preventDefault()
+        let {name,username,password,email}=state
+        if(!name||!username||!password||!email){
+           toast.error("all fields are mandatory")
+          return
+        }
+       let {validateAll,getAllValidationErrorMessage}=validatePassword(password)
+        if(!validateAll()){
+          toast.error(`${getAllValidationErrorMessage}`)
+        }
+
+        if(!repeatPassword){
+          toast.error("password and confirm password did not match")
+          return
+          
+        }
         console.log(state);
         
       }
+
+       let handlePasswordCheck=(e)=>{
+        setReapeatePassword(e.target.value)
+        console.log(repeatPassword)
+        
+        if(state.password!=e.target.value){
+          e.target.style.borderBottom="2px solid red"
+        }else{
+          e.target.style.borderBottom="2px solid white"
+        }
+      }
   return (
     <div className='bg-[#efefef] size-full flex justify-center items-center rounded-2xl ' >
-      <form action="" className='bg-whight h-[90%]  w-1/2 flex justify-center items-center flex-col gap-8 px-[80px] py-40 shadow-2xl rounded-3xl max-md:w-[90%]' onSubmit={handleSubmit}>
+      <form action="" className='bg-whight h-[90%]  w-1/2 flex justify-center items-center flex-col gap-8 px-[80px] py-40 shadow-2xl rounded-3xl max-md:w-[90%] overflow-hidden' onSubmit={handleSubmit}>
         <div className='font-bold  flex justify-center items-center text-3xl max-md:w-sm'>
           <h1 >Registration Form</h1>
         </div>
@@ -51,13 +88,19 @@ const Register = () => {
           <span><TbPasswordFingerprint /></span>
         </div>
 
+        <div className={errorMessage?'w-full flex justify-center items-center px-3 rounded-sm':'hidden'}>
+          <span className='text-red-500'>*{errorMessage}</span>
+        </div>
+
         <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="password" name="" placeholder='Enter repeate password' className='w-full outline-none px-4 h-10'/>
+          <input type="password" name="" placeholder='Enter repeate password' className='w-full outline-none px-4 h-10' onChange={handlePasswordCheck}/>
           <span><TbLockPassword /></span>
         </div>
 
-        <div className='border-2 w-full flex justify-center items-center px-3 rounded-md bg-black '>
-          <button className='w-full outline-none px-4 h-10 text-white font-bold'>Click</button>
+        
+
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-md bg-black hover:bg-[#555] active:bg-amber-400]'>
+          <button className='w-full outline-none px-4 h-10 text-white font-bold '>Click</button>
         </div>
       </form>
     </div>
